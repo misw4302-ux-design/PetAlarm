758637fb1217d810b0b2a2a2b39e2448187be58b
gcastrot82 <gcastrot82@hotmail.com>
2023-03-13 19:51:18 -0500

    Update main menu

--- Mobil/app/src/main/java/com/example/petalarmmobil/MainActivity.kt
+++ Mobil/app/src/main/java/com/example/petalarmmobil/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.petalarmmobil
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnRegistro: Button
+
+    private lateinit var btnIngresar: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        btnRegistro = findViewById(R.id.btn_registrarse)
+        btnIngresar = findViewById(R.id.btn_ingresar)
+
+        btnRegistro.setOnClickListener {
+            setContentView(R.layout.activity_registro)
+        }
+
+
+        btnIngresar.setOnClickListener {
+            setContentView(R.layout.activity_main_menu)
+        }
+
+
+
     }
 }
--- Mobil/app/src/main/java/com/example/petalarmmobil/MainMenu.kt
+++ Mobil/app/src/main/java/com/example/petalarmmobil/MainMenu.kt
@@ -1,27 +1,20 @@
 package com.example.petalarmmobil
 
-import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageView
 
-class Registro : AppCompatActivity() {
+class MainMenu : AppCompatActivity() {
 
     private lateinit var btnCerrar: ImageView
-
-    private lateinit var btnRegistred: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registro)
+        setContentView(R.layout.activity_main_menu)
 
         btnCerrar = findViewById(R.id.btn_cerrar)
 
-        btnRegistred = findViewById(R.id.btn_registred)
-
-        btnRegistred.setOnClickListener {
+        btnCerrar.setOnClickListener {
             setContentView(R.layout.activity_main)
         }
-
     }
 }

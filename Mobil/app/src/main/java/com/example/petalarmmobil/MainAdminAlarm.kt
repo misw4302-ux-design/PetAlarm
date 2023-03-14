1a11adce5f0892acb597525521d57f60bf985d81
Jenny Idarraga <j.idarragar@uniandes.edu.co>
2023-03-14 00:18:42 -0500

    Update vista Adminalarma

---
+++ Mobil/app/src/main/java/com/example/petalarmmobil/MainAdminAlarm.kt
@@ -0,0 +1,11 @@
+package com.example.petalarmmobil
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainAdminAlarm : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_admin_alarm)
+    }
+}
--- Mobil/app/src/main/java/com/example/petalarmmobil/MainMenu.kt
+++ Mobil/app/src/main/java/com/example/petalarmmobil/MainMenu.kt
@@ -1,20 +1,34 @@
 package com.example.petalarmmobil
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 
 class MainMenu : AppCompatActivity() {
 
     private lateinit var btnCerrar: ImageView
+
+    private lateinit var btn_gestor_alarmas: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
 
         btnCerrar = findViewById(R.id.btn_cerrar)
 
+        btn_gestor_alarmas = findViewById(R.id.btn_gestor_alarmas)
+
         btnCerrar.setOnClickListener {
             setContentView(R.layout.activity_main)
         }
+
+        fun onClick(view: View) {
+            val intent = Intent(this, MainAdminAlarm::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }

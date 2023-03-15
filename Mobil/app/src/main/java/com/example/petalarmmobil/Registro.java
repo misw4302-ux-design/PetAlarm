18d5ab0aee5684f9bc003510a793ecec55edf6b4
Jenny Idarraga <j.idarragar@uniandes.edu.co>
2023-03-15 00:37:49 -0500

    Update Mobil en java

---
+++ Mobil/app/src/main/java/com/example/petalarmmobil/Registro.java
@@ -0,0 +1,21 @@
+package com.example.petalarmmobil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+    }
+
+    public void inicio(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}

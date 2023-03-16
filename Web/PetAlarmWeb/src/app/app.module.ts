0fd294682e9d40b40e78fcff50c6ffa49b53861d
gcastrot82 <gcastrot82@hotmail.com>
2023-03-15 22:46:28 -0500

    Update

---
+++ Web/PetAlarmWeb/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { GestorAlarmasComponent } from './gestor-alarmas/gestor-alarmas.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    GestorAlarmasComponent,
+
+   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
